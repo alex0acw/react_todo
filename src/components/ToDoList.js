ee5941b15d3873696917fd26d8c3822f6189a718
acw <alex_acw@outlook.com>
2020-12-08 17:21:40 +0800

    [todolist][alex&david] feat: add todolist

---
+++ src/components/ToDoList.js
@@ -0,0 +1,11 @@
+import ToDoGeneratorContainer from "../containers/ToDoGeneratorContainer";
+import ToDoGroupContainer from "../containers/ToDoGroupContainer";
+
+export default function () {
+    return (
+        <div>
+            <ToDoGeneratorContainer />
+            <ToDoGroupContainer />
+        </div>
+    )
+}
